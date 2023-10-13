9d880bd02daec9036913f4386fdb6feae4e74203
许磊 <xulei131401@outlook.com>
2023-10-13 21:58:42 +0800

    fix

--- server.mk
+++ server.mk
@@ -23,19 +23,19 @@ build-server:
 	echo "Build server success ..."
 
 build-server-win:
-	GOOS=windows
+	@$(BUILD_PARAMS) GOOS=windows
 	@cd $(SRC_PATH) && $(GO_BUILD) -ldflags="-s -w" -o $(OUT_PATH)/$(WINDOWS_BIN_NAME) ./$(MAIN_GO_FILE_NAME) && \
 	$(WINDOWS_UPX) && \
 	echo "Build server windows success ..."
 
 build-server-linux:
-	GOOS=linux
+	@$(BUILD_PARAMS) GOOS=linux
 	@cd $(SRC_PATH) && $(GO_BUILD) -ldflags="-s -w" -o $(OUT_PATH)/$(LINUX_BIN_NAME) ./$(MAIN_GO_FILE_NAME) && \
 	$(LINUX_UPX) && \
 	echo "Build server linux success ..."
 
 build-server-darwin:
-	GOOS=darwin
+	@$(BUILD_PARAMS) GOOS=darwin
 	@cd $(SRC_PATH) && $(GO_BUILD) -ldflags="-s -w" -o $(OUT_PATH)/$(DARWIN_BIN_NAME) ./$(MAIN_GO_FILE_NAME) && \
 	$(DARWIN_UPX) && \
 	echo "Build server darwin success ..."
